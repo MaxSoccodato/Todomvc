363f3557d82d30d0565c131094b14d5ea0ed36ff
Maxime Soccodato <maxime.soccodato@epitech.eu>
2023-03-24 15:55:00 +0100

    toggle hidden (still buggy) and check all complete

--- src/app/todos/components/main/main.component.ts
+++ src/app/todos/components/main/main.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { TodosService } from 'app/todos/services/todos.service';
 import { FilterEnum } from 'app/todos/types/filter.enum';
 import { TodoInterface } from 'app/todos/types/todo.interface';
-import { combineLatest, Observable } from 'rxjs';
-import {map} from 'rxjs/operators'
+import { combineLatest, Observable, pipe } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-todos-main',
@@ -11,23 +11,38 @@ import {map} from 'rxjs/operators'
 })
 export class MainComponent implements OnInit {
   visibleTodos$: Observable<TodoInterface[]>;
+  noTodoClass$: Observable<Boolean>;
+  isAllTodosSelected$: Observable<Boolean>;
 
   constructor(private todosService: TodosService) {
-    //combine Latest combine streams 
+    this.isAllTodosSelected$ = this.todosService.todos$.pipe(
+      map((todos) => todos.every((todo) => todo.isCompleted))
+    );
+    this.noTodoClass$ = this.todosService.todos$.pipe(
+      map((todos) => todos.length === 0)
+    );
+    console.log("notodoclass", this.noTodoClass$)
+    //combine Latest combine streams
     this.visibleTodos$ = combineLatest([
       this.todosService.todos$,
-      this.todosService.filter$
+      this.todosService.filter$,
       //Pipe let us call functions when combining streams
-    ]).pipe(map(([todos, filter]:  [TodoInterface[], FilterEnum]) => {
-      console.log('combine', todos, filter);
-      if (filter === FilterEnum.active){
-        return todos.filter((todo) => !todo.isCompleted)
-      } else if (filter === FilterEnum.completed) {
-        return todos.filter((todo) => todo.isCompleted)
-      }
-      return todos;
-    }))
-   }
-  ngOnInit() { }
+    ]).pipe(
+      map(([todos, filter]: [TodoInterface[], FilterEnum]) => {
+        console.log('combine', todos, filter);
+        if (filter === FilterEnum.active) {
+          return todos.filter((todo) => !todo.isCompleted);
+        } else if (filter === FilterEnum.completed) {
+          return todos.filter((todo) => todo.isCompleted);
+        }
+        return todos;
+      })
+    );
+  }
 
+  toggleAllTodos(event: Event) : void{
+    const target = event.target as HTMLInputElement
+    this.todosService.toggleAll(target.checked)
+  }
+  ngOnInit() {}
 }
