f2b4fe366e4b5e4457d3eb9e1389c0308a29be74
Maxime Soccodato <maxime.soccodato@epitech.eu>
2023-03-24 15:55:29 +0100

    hidden (still buggy) and toggle complete all

--- src/app/todos/services/todos.service.ts
+++ src/app/todos/services/todos.service.ts
@@ -4,19 +4,30 @@ import { FilterEnum } from '../types/filter.enum';
 import { TodoInterface } from '../types/todo.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodosService {
   todos$ = new BehaviorSubject<TodoInterface[]>([]);
-  filter$ = new BehaviorSubject<FilterEnum>(FilterEnum.all)
+  filter$ = new BehaviorSubject<FilterEnum>(FilterEnum.all);
 
-  addTodo(text : string ): void {
-    const newTodo : TodoInterface = {
+  addTodo(text: string): void {
+    const newTodo: TodoInterface = {
       text,
-      isCompleted : false,
-      id: Math.random().toString(16)
+      isCompleted: false,
+      id: Math.random().toString(16),
     };
-    const updatedTodos = [...this.todos$.getValue(), newTodo]
+    const updatedTodos = [...this.todos$.getValue(), newTodo];
     this.todos$.next(updatedTodos);
+  }
+
+  toggleAll(isCompleted: boolean): void {
+    console.log('iscompleted', isCompleted)
+    const updateTodos = this.todos$.getValue().map((todo) => {
+      return {
+        ...todo,
+        isCompleted : isCompleted
+      };
+    });
+    this.todos$.next(updateTodos)
   }
 }
